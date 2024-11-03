4062e4d17bc63340f3cb0ccf11b36019974d1a20
Arnaud Champion <arnaudchampion76@gmail.com>
2024-11-03 16:46:03 +0700

    Create new project front project from scratch

---
+++ querier/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:querier/const.dart';
+import 'package:querier/dashboard.dart';
+import 'package:querier/login_screen.dart';
+import 'package:querier/login_bloc.dart';
+
+void main() {
+  runApp(QuerierApp());
+}
+
+class QuerierApp extends StatelessWidget {
+  const QuerierApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Querier',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: MaterialColor(
+            primaryColorCode,
+            <int, Color>{
+              50: const Color(primaryColorCode).withOpacity(0.1),
+              100: const Color(primaryColorCode).withOpacity(0.2),
+              200: const Color(primaryColorCode).withOpacity(0.3),
+              300: const Color(primaryColorCode).withOpacity(0.4),
+              400: const Color(primaryColorCode).withOpacity(0.5),
+              500: const Color(primaryColorCode).withOpacity(0.6),
+              600: const Color(primaryColorCode).withOpacity(0.7),
+              700: const Color(primaryColorCode).withOpacity(0.8),
+              800: const Color(primaryColorCode).withOpacity(0.9),
+              900: const Color(primaryColorCode).withOpacity(1.0),
+            },
+          ),
+          scaffoldBackgroundColor: const Color(0xFF171821),
+          fontFamily: 'IBMPlexSans',
+          brightness: Brightness.dark),
+      home: MultiBlocProvider(providers: [
+        BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(),
+        ),
+      ], child: const LoginScreen()),
+    );
+  }
+}
+//add connection page in this code
+//add UI in different page..
